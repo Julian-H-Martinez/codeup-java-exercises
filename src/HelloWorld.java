9e4f3f95b240159f45ab1c207f0070139417f7d8
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-12-14 15:31:06 -0600

    problems2-9 completed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -78,32 +78,66 @@ public class HelloWorld {
 //        int difference = (byte)(x-y);   //  EXPLICIT
 //        System.out.println(difference);
 
-/**  Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console. */
+/** 1) Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console. */
         int myFavoriteNumber = 10;
         System.out.println(myFavoriteNumber);
 
- /** Create a String variable named myString and assign a string value to it, then print the variable out to the console.*/
+/** 2) Create a String variable named myString and assign a string value to it, then print the variable out to the console.*/
         String myString = "So far so good here in JAVA world!";
         System.out.println(myString);
 
-/**
- * Change your code to assign a character value to myString. What do you notice?
- *
- * Change your code to assign the value 3.14159 to myString. What happens?
- *
- * Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
- *
- * Change your code to assign the value 3.14 to myNumber. What do you notice?
- *
- * Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
- *
- * Change your code to assign the value 123 to myNumber.
- *
- * Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
- *
- * Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
- *
- * Copy and paste the following code blocks one at a time and execute them
- */
+/** 3) Change your code to assign a character value to myString. What do you notice?*/
+//        char myString = "So far so good here in JAVA world!";     //  won't accept it
+
+/** 4) Change your code to assign the value 3.14159 to myString. What happens?*/
+//        String myString = 3.14159;    //  it won't accept it
+
+/** 5) Declare a long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens? */
+//        long myNumber;
+//        System.out.println(myNumber);   //  get a red line on myNumber in sout
+
+/** 6) Change your code to assign the value 3.14 to myNumber. What do you notice? */
+//        myNumber = 3.14;    //  it's not being accepted; not assignable?
+
+/** 7) Change your code to assign the value 123L (Note the 'L' at the end) to myNumber. */
+//        myNumber = 123L;    //  it's accepted and is assignable with 'L' at end
+
+/** 8) Change your code to assign the value 123 to myNumber.
+ * Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does? */
+//        myNumber = 123;     //  it's accepted now; hmmmm
+//  because of the decimal; long and int are both whole numbers*/
+
+
+/** 9) Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this? */
+//        float myNumber = 3.14;  //  not accepting
+//        float myNumber = 3.14f; //  first way to fix it
+//        float myNumber = (float)(3.14);   //  second way to fix it
+//        System.out.println(myNumber);
+
+/** 10) Copy and paste the following code blocks one at a time and execute them */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
