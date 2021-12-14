0f8d429126d638d5507aed782a3fa32ed9b49160
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-12-14 15:06:31 -0600

    completed lec notes; starting on Syntax, Types, & Variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,108 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//Class Lecture
+////        System.out.println("Hello");
+////        System.out.println("World!");
+//
+//        byte month = 12;
+////        System.out.println(month);
+//
+//        short daysInYear = 365;
+////        System.out.println(daysInYear);
+//
+////        System.out.println('I');
+//
+//        //  New line
+////        System.out.println("Hello\nWorld!");
+//        //  tab
+////        System.out.println("Hello\tWorld!");
+//        //  backslash
+////        System.out.println("Hello\\World!");
+//
+//        String greeting = "Hello world of JAVA!!!";
+//
+//        String name;
+//        name = "Jay";
+////        System.out.println(greeting);
+////        System.out.println(name);
+//
+//        //  need f at end to denote a float
+//        float pi = 3.14f;
+////        System.out.println(pi);
+//
+//        //  JAVA we use final which is similar to const in es6
+//        final String APIKEY = "dfjkle;qadfujficox;jv5659+692dafe";
+//
+//        //  APIKEY = "ballaballdfadjfkl;edqafeadfjkdal;fje"  //   unable to set final with new value
+//
+//        int firstNumber = 20;
+//        int secondNumber = 22;
+////        System.out.println(firstNumber + secondNumber); //  addition
+////        System.out.println(firstNumber / secondNumber); //  division
+////        System.out.println(firstNumber * secondNumber); //  multiplication
+////        System.out.println(firstNumber % secondNumber); //  modulus
+////        System.out.println(firstNumber - secondNumber); //  subtraction
+//
+//        int postIncrement = 0;
+//        postIncrement++;
+////        increment++;
+//        System.out.println(postIncrement);
+//        int decrement = --postIncrement;
+//        System.out.println(decrement);
+//        decrement--;
+//        System.out.println(decrement);
+//
+//        decrement += 5;
+//        System.out.println(decrement);
+//
+//        //  CASTING
+//
+//        byte dayOfMonth = 14;
+//        byte birthday = 24;
+//
+//        //  returns the correct output because the result of the expression \by default is an int data type
+//        System.out.println(dayOfMonth * birthday);
+//        //  cannot assign result to a byte data type, because the result is out of scope.
+//        //  byte result = dayOfMonth * birthday;
+//
+//        //  IMPLICIT CASTING = less specific data type, to a more specific data type
+//        int result = dayOfMonth * birthday;
+//
+//        //  EXPLICIT CASTING = more specific data type, to less specific data type
+//        int x = 900;
+//        int y = 800;
+//
+//        //  byte difference = x - y;    //  IMPLICIT
+//
+//
+//        int difference = (byte)(x-y);   //  EXPLICIT
+//        System.out.println(difference);
+
+/**  Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console. */
+        int myFavoriteNumber = 10;
+        System.out.println(myFavoriteNumber);
+/**
+ *
+ *
+ * Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+ *
+ * Change your code to assign a character value to myString. What do you notice?
+ *
+ * Change your code to assign the value 3.14159 to myString. What happens?
+ *
+ * Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+ *
+ * Change your code to assign the value 3.14 to myNumber. What do you notice?
+ *
+ * Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+ *
+ * Change your code to assign the value 123 to myNumber.
+ *
+ * Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+ *
+ * Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+ *
+ * Copy and paste the following code blocks one at a time and execute them
+ */
     }
 }
