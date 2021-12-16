ceec88144aaef68adbceffdf2d43bc0504bb8b82
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-12-16 09:24:35 -0600

    morning warm-up complete

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Warmups {
     public static void main(String[] args){
         System.out.println("Java is statically typed, while Javascript is dynamically typed.");
+
+        //  12.16 Warmup
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What did you eat for breakfast today?");
+        String userBreakfast = sc.nextLine();
+        System.out.printf("Wow, you ate %s for breakfast? How was the %s", userBreakfast, userBreakfast);
     }
 }
