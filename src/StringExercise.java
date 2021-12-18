09a4d0c864fb416864f3d73b35808e10cc6bc1b4
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-12-18 12:17:26 -0600

    String Basics completed

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,13 +1,26 @@
 public class StringExercise {
     public static void main(String[] args){
         System.out.println("Yep I work!");
-
-
-
-
-
-
-
+        /** 1) String Basics */
+        //  Create a class named StringExercise with a main method.
+        //
+        //  For each of the following output examples: create a String variable that contains the desired output and print it out to the console,
+        //  you can do this with only one String variable and one print statement for each output example.
+        //
+        String message = "We don't need no ";
+        System.out.println(message + "education\n" + message + "thought control\n");
+        //  We don't need no education
+        //  We don't need no thought control
+        //
+        System.out.println("Check \"this\" out!, \"s inside of \"s!");
+        //  Check "this" out!, "s inside of "s!
+        //
+        System.out.println("In windows, the main drive is usually C:\\");
+        //  In windows, the main drive is usually C:\
+        //
+        System.out.println("I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!");
+        //  I can do backslashes \, double backslashes \\,
+        //  and the amazing triple backslash \\\!
 
 
     }
