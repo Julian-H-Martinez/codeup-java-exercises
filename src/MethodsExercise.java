c315d03d283e8d5d52f35d11632f7fda87ff710d
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-03 22:58:35 -0600

    problem2 completed

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercise {
 
     public static int addition(int num, int num2){
@@ -26,20 +28,49 @@ public class MethodsExercise {
     public static int remainder(int num, int num2){
         return num % num2;
     }
+/*
+Create a method that validates that user input is in a certain range
+
+The method signature should look like this:
+
+public static int getInteger(int min, int max);
+and is used like this:
+
+System.out.print("Enter a number between 1 and 10: ");
+int userInput = getInteger(1, 10);
+If the input is invalid, prompt the user again.
+
+Hint: recursion might be helpful here!
+*/
+
+    public static int getInteger(int min, int max){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pick a number between 1 and 10: ");
+        int userInput = sc.nextInt();
+        if(userInput < min){
+            System.out.println("That's below the range; please select between 1 and 10.");
+        }else if(userInput > max){
+            System.out.println("That's above the range; please select between 1 and 10.");
+        }else{
+            return userInput;
+        }
+        System.out.println("You chose " + userInput);
+        return getInteger(min, max);
+    }
     public static void main(String[] args) {
 //        System.out.println("Yep I work");
 //        System.out.println(addition(4, 5));               //  should be 9
 //        System.out.println(subtraction(10, 3));           //  should be 7
-        System.out.println(multiplication(3, 4));         //  should be 420
+//        System.out.println(multiplication(3, 4));         //  should be 420
 //        System.out.println(division(23, 5));              //  should be 4
 //        System.out.println(division(23, 0));              //  anything divided by 0 is undefined causing a runtime error
 //        System.out.println(remainder(23, 5));             //  should be 3
+//        getInteger(1, 10);
 
     }
 }
 /*
 Bonus
-
 Create your multiplication method without the * operator (Hint: a loop might be helpful).
 Do the above with recursion.
  */
