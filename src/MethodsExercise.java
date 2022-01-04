fb8e11ac855f1b0303e5e2ccbd2c473b86a2c754
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-03 18:58:42 -0600

    completed problem1 without bonus; will complete bonus on return

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,7 +1,34 @@
 public class MethodsExercise {
 
+    public static int addition(int num, int num2){
+        return num + num2;
+    }
+    public static int subtraction(int num, int num2){
+        return num - num2;
+    }
+    public static int multiplication(int num, int num2){
+        return num * num2;
+    }
+    public static int division(int num, int num2){
+        return num/num2;
+    }
+    public static int remainder(int num, int num2){
+        return num % num2;
+    }
     public static void main(String[] args) {
 //        System.out.println("Yep I work");
+//        System.out.println(addition(4, 5));             //  should be 9
+//        System.out.println(subtraction(10, 3));         //  should be 7
+//        System.out.println(multiplication(2, 210));     //  should be 420
+//        System.out.println(division(23, 5));            //  should be 4
+//        System.out.println(division(23, 0));            //  anything divided by 0 is undefined causing a runtime error
+//        System.out.println(remainder(23, 5));           //  should be 3
 
     }
 }
+/*
+Bonus
+
+Create your multiplication method without the * operator (Hint: a loop might be helpful).
+Do the above with recursion.
+ */
