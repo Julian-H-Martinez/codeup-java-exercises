70565c118b72d4f01ca1beb9bd975891bcb662a0
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-03 22:28:28 -0600

    putting recursion on hold

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -13,11 +13,13 @@ public class MethodsExercise {
             total += num;
             i++;
         }while(i <= num2);
-//        for (int i = 1; i <= num2; i++){
-//            total += num;
-//        }
         return total;
     }
+//    public static int multiplication(int num, int num2){
+//        int total = 0;
+//            total += num;
+//        return multiplication(total, num2);
+//    }
     public static int division(int num, int num2){
         return num/num2;
     }
@@ -26,12 +28,12 @@ public class MethodsExercise {
     }
     public static void main(String[] args) {
 //        System.out.println("Yep I work");
-//        System.out.println(addition(4, 5));             //  should be 9
-//        System.out.println(subtraction(10, 3));         //  should be 7
-        System.out.println(multiplication(3, 4));     //  should be 420
-//        System.out.println(division(23, 5));            //  should be 4
-//        System.out.println(division(23, 0));            //  anything divided by 0 is undefined causing a runtime error
-//        System.out.println(remainder(23, 5));           //  should be 3
+//        System.out.println(addition(4, 5));               //  should be 9
+//        System.out.println(subtraction(10, 3));           //  should be 7
+        System.out.println(multiplication(3, 4));         //  should be 420
+//        System.out.println(division(23, 5));              //  should be 4
+//        System.out.println(division(23, 0));              //  anything divided by 0 is undefined causing a runtime error
+//        System.out.println(remainder(23, 5));             //  should be 3
 
     }
 }
