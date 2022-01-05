2a47d836e5c44c013ff6720cf578dd0cd0feb34d
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-05 10:54:03 -0600

    completed Understanding References portion

--- src/Person.java
+++ src/Person.java
@@ -26,10 +26,29 @@ public class Person {
 
     public static void main(String[] args){
 //        System.out.println("Yep I'm working!");
-        Person julian = new Person();
+//        Person julian = new Person();
 //        System.out.println(julian);
-        julian.setName("Julian");
-        System.out.println(julian.name);
-        julian.sayHello();
+//        julian.setName("Julian");
+//        System.out.println(julian.name);
+//        julian.sayHello();
+
+        //  UNDERSTANDING REFERENCES
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));    //  true
+//        System.out.println(person1 == person2);                             //  false
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);         //  true
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());          //  John
+//        System.out.println(person2.getName());          //  John
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());          //  John -> Jane, how?
+//        System.out.println(person2.getName());          //  Jane
     }
 }
