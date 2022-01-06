f6a0b1f851c1443c7ef933b80913c813c2cac321
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-06 10:42:27 -0600

    removed 01.06 warmup

--- src/Warmups.java
+++ src/Warmups.java
@@ -2,102 +2,6 @@ import java.util.Scanner;
 
 public class Warmups {
 
-    /* 1) Create a class named “Penguin” */
-    public static class Penguins{
-        /* FIELDS */
-        /* 2) Give your class the following private fields
-            Name
-            Weight
-            Species
-            Sex
-        */
-        private String name;
-        private double weight;
-        private String species;
-        private String sex;
-
-        /* METHODS */
-
-        /* 3) Give your class the following public methods
-            getName
-            getWeight
-            getSpecies
-            getSex
-            setName
-            setWeight
-            setSpecies
-            setSex
-        */
-        /* GETTERS/SETTERS */
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public double getWeight() {
-            return weight;
-        }
-
-        public void setWeight(double weight) {
-            this.weight = weight;
-        }
-
-        public String getSpecies() {
-            return species;
-        }
-
-        public void setSpecies(String species) {
-            this.species = species;
-        }
-
-        public String getSex() {
-            return sex;
-        }
-
-        public void setSex(String sex) {
-            this.sex = sex;
-        }
-
-//        @Override
-//        public String toString() {
-//            return "Penguins{\n" +
-//                    "name='" + name + '\'' +
-//                    ", \nweight=" + weight +
-//                    ", \nspecies='" + species + '\'' +
-//                    ", \nsex='" + sex + '\'' +
-//                    '}';
-//        }
-
-/* CONSTRUCTOR(S) */
-        /* 4) Give your class a constructor method */
-        public Penguins(){
-            System.out.println("Creating a new Penguin");
-        }
-
-        public Penguins(String name, double weight, String species, String sex) {
-            this.name = name;
-            this.weight = weight;
-            this.species = species;
-            this.sex = sex;
-        }
-
-
-        /* PUBLIC STATIC METHODS */
-        /* 5) Give your class the following public static methods
-            canFly (returns false)
-            areEvil (returns true) [Source]
-         */
-        public static boolean canFly(){
-            return false;
-        }
-        public static boolean areEvil(){
-            return true;
-        }
-    }
-
 //    public static String startDay(){
 //        return "Brain loading... Done!";
 //    }
@@ -116,7 +20,5 @@ public class Warmups {
         //  Add, commit, and push your work to GitHub.
 
 //        System.out.println(startDay());
-        Penguins bob = new Penguins("bob", 200, "non-flying bird", "male");
-        System.out.println(bob.toString());
     }
 }
