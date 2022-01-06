b4f76797782a35f000266208ffe52e9f11e98b94
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-05 23:16:42 -0600

    refactored some methods

--- src/util/Input.java
+++ src/util/Input.java
@@ -17,7 +17,6 @@ public class Input {
 
 //  METHOD(S)
     public String getString(){
-        System.out.println("Enter a string: ");
         this.userString = scanner.nextLine();
 
         System.out.println(userString);
@@ -26,7 +25,6 @@ public class Input {
 
     /*The yesNo method should return true if the user enters y, yes, or variants thereof, and false otherwise.*/
     public boolean yesNo(){
-        System.out.println("You are a sports fan. [YES/NO]");
         this.userString = scanner.nextLine();
         if(userString.equalsIgnoreCase("y") || userString.equalsIgnoreCase("yes")){
             return true;
@@ -36,9 +34,9 @@ public class Input {
     }
 
     public int getInt(int min, int max){
-        System.out.printf("Enter a number between %d-%d: ", min, max);
         this.userInt = scanner.nextInt();
         if(userInt < min || userInt > max){
+            System.out.printf("Enter a number between %d-%d: ", min, max);
             return getInt(min, max);
         }else{
             return userInt;
@@ -50,9 +48,9 @@ public class Input {
     }
 
     public double getDouble(int min, int max){
-        System.out.printf("Enter a decimal between %d - %d: ", min, max);
         this.userDouble = scanner.nextDouble();
         if(userDouble <= min || userDouble >= max){
+            System.out.printf("Enter a decimal between %d - %d: ", min, max);
             return getDouble(min, max);
         }else{
             return userDouble;
@@ -67,11 +65,16 @@ public static class InputTest{
     public static void main(String[] args) {
 
         Input user1 = new Input();
+//        System.out.println(user1.getInt());
+//        System.out.println("Enter a string: ");
 //        System.out.println(user1.getString());
 //        String confirm = user1.getString();
+//        System.out.println("You are a sports fan. [YES/NO]");
 //        System.out.println(user1.yesNo());
+//        System.out.println("Enter a number: " );
 //        user1.getInt(3, 10);
 //        System.out.println(user1.getInt());
+//        System.out.println("Enter a decimal : ");
 //        user1.getDouble(0, 1);
 //        System.out.println(user1.getDouble());
     }
