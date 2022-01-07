13914eba73f095e8a18c61d5da5ad71e6fbb4dc9
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-07 16:04:08 -0600

    added ShapesTest class to test methods

---
+++ src/shapes/ShapesTest.java
@@ -0,0 +1,10 @@
+package shapes;
+
+public class ShapesTest {
+    public static void main(String[] args) {
+        Rectangle rec1 = new Rectangle(3, 4);
+        System.out.println(rec1.getArea());
+        System.out.println(rec1.getPerimeter());
+
+    }
+}
