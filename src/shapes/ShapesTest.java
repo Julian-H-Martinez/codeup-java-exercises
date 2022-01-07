50fdbf1a59b676adbf0f848924a0e59c316994f6
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-07 17:03:11 -0600

    working on overriding methods.

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,9 +2,22 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle rec1 = new Rectangle(3, 4);
-        System.out.println(rec1.getArea());
-        System.out.println(rec1.getPerimeter());
+//        Rectangle rec1 = new Rectangle(3, 4);
+//        System.out.println(rec1.getArea());
+//        System.out.println(rec1.getPerimeter());
 
+//        Rectangle box1 = new Rectangle(5, 4);
+//        System.out.println(box1.getArea());
+//        System.out.println(box1.getPerimeter());
+
+        Rectangle box2 = new Square(5);
+//        System.out.println(box2.getArea());
+        box2.getArea();
+        System.out.println("I'm sout: " + box2.getPerimeter());
+
+        Rectangle box3 = new Square(4);
+//        System.out.println(box3.getArea());
+        box3.getArea();
+        System.out.println("I'm sout: " + box3.getPerimeter());
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,4 +1,38 @@
 package shapes;
+/*
+In the Square class, override the getArea and getPerimeter methods with the following definitions for a square
+perimeter = 4 x side
+area = side ^ 2
 
+ */
 public class Square extends Rectangle{
+    protected double side;
+//
+//    public double getSide() {
+//        return side;
+//    }
+//
+//    public void setSide(double side) {
+//        this.side = side;
+//    }
+
+    public Square(){
+        System.out.println("Creating a square");
+    }
+    public Square(double side){
+        super(side, side);
+    }
+
+    public double getArea(){
+//        super.getArea();
+        System.out.println("I'm overriding: " + Math.pow(this.side, 2));
+        return Math.pow(this.side, 2);
+    }
+
+    public double getPerimeter(){
+        super.getPerimeter();
+        return Math.pow(side, 2);
+    }
+
+
 }
