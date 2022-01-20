7ffed1d7a9694b0e028571c9b3a8334cc77db4bf
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-20 17:09:46 -0600

    added InputTest class

--- src/util/Input.java
+++ src/util/Input.java
@@ -124,28 +124,4 @@ public class Input {
             return userDouble;
         }
     }
-
-    public static class InputTest {
-        public static void main(String[] args) throws Exception {
-
-            Input user1 = new Input();
-//        System.out.println("Enter a number");
-//            user1.getInt(3, 10);
-//        System.out.println("Enter a string: ");
-//        System.out.println(user1.getString());
-//        user1.getString("What's your favorite quote? ");
-//        String confirm = user1.getString();
-//        System.out.println("You are a sports fan. [YES/NO]");
-//        System.out.println(user1.yesNo());
-//        System.out.println("Enter a number: " );
-//        user1.getInt(3, 10, "Enter a number: ");
-//        System.out.println(user1.getInt());
-//        System.out.println("Enter a decimal : ");
-//        user1.getDouble(0, 1);
-//        System.out.println(user1.getDouble());
-//        user1.yesNo("Would you like to continue?");
-//            user1.getBinary();
-//            user1.getHex();
-        }
-    }
 }
---
+++ src/util/InputTest.java
@@ -0,0 +1,26 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args){
+
+        Input user1 = new Input();
+//        System.out.println("Enter a number");
+//            user1.getInt(3, 10);
+//        System.out.println("Enter a string: ");
+//        System.out.println(user1.getString());
+//        user1.getString("What's your favorite quote? ");
+//        String confirm = user1.getString();
+//        System.out.println("You are a sports fan. [YES/NO]");
+//        System.out.println(user1.yesNo());
+//        System.out.println("Enter a number: " );
+//        user1.getInt(3, 10, "Enter a number: ");
+//        System.out.println(user1.getInt());
+//        System.out.println("Enter a decimal : ");
+//        user1.getDouble(0, 1);
+//        System.out.println(user1.getDouble());
+//        user1.yesNo("Would you like to continue?");
+//            user1.getBinary();
+//            user1.getHex();
+    }
+}
+
